6dcaf0e77dbfc87e656a41f6adf9d971687e3c08
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2020-03-27 09:16:46 +0300

    .env add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FloorsModule } from './modules/floors.module';
 
+require('dotenv').config();
+
 @Module({
   imports: [
     FloorsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: "schema.gql",
     }),
-    MongooseModule.forRoot('mongodb+srv://administrator:God291286@cluster0-g5ppm.azure.mongodb.net/warmehaus_dto?retryWrites=true&w=majority')
+    MongooseModule.forRoot(process.env.DB_CONNECTION_STRING)
   ],
   controllers: [],
   providers: [],
